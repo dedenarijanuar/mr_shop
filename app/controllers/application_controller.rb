d878689daa9542c8ecf1b04538f4dd63c66b6a60
dedenarijanuar <dedenarijanuar@gmail.com>
2019-12-16 03:10:40 +0000

    fixing get data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
 	before_action :quotation_adds
 
 	def quotation_adds
+session['spareparts'] = [] unless session['spareparts']
+session['labours'] = [] unless session['labours']
+session['discount'] = [] unless session['discount']
+
+#session['spareparts'] = []
+#session['labours'] = []
+#session['discount'] = nil
+
 		@add_spareparts = session['spareparts']
 		@add_labours = session['labours']
 		@add_discount = session['discount']
